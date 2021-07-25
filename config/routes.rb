118207be96af8aeae2ed01940d1cda8a68f01450
tateyama-hiroaki <user@USERnoMacBook-Air.local>
2021-07-25 09:33:20 +0900

    Created book add page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/index',   to: 'books#index'
   get '/home',    to: 'static_pages#home'
   get '/profile', to: 'static_pages#profile'
+  get 'book_add', to: 'books#new'
 end
