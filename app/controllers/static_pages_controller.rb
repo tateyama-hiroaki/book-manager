e5d6fce12f8866b56328362ab9731038a74e2d29
tateyama-hiroaki <user@USERnoMacBook-Air.local>
2021-08-09 10:49:22 +0900

    Fixed home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    @books_count = Book.count
   end
 
   def search
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
-  mount_uploader :image, ImageUploader
+  
+  mount_uploader :image, ImageUploader  
 end
